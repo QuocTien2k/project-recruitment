d2fb33004fd5a1905f06fa0fe8403f3971a3c515
QuocTien2k <hadesdqt@gmail.com>
2025-05-09 11:21:19 +0700

    create model User.js

---
+++ server/models/User.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    middleName: { type: String, required: true },
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    phone: { type: String, required: true },
+    password: { type: String, required: true },
+    profilePic: {
+      url: {
+        type: String,
+        required: false,
+      },
+      public_id: {
+        type: String,
+        required: false,
+      },
+    },
+    resetPasswordToken: {
+      type: String,
+      default: null,
+      select: false, // không cần trả về khi query user
+    },
+    role: { type: String, enum: ["user", "teacher", "admin"], default: "user" },
+    district: { type: String, required: true },
+    province: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
