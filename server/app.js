0e31972c1765bbe522b79fcb6790404a2c4679a7
QuocTien2k <hadesdqt@gmail.com>
2025-05-09 10:43:11 +0700

    update config app.js

--- server/app.js
+++ server/app.js
@@ -2,23 +2,16 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-// Cho phép CORS với frontend (React) middleware
+// Cho phép CORS từ frontend React
 app.use(
   cors({
-    origin: "http://localhost:5173", // Chỉ cho phép React frontend
-    credentials: true, // Nếu có gửi cookies hoặc token
+    origin: "http://localhost:5173",
+    credentials: true, // cho phép cookie từ frontend
   })
 );
-app.use(cors());
 
-// giải mã urlencoded body từ client gửi lên (dạng form)
-app.use(express.urlencoded({ extended: true }));
+// Giải mã JSON và form-urlencoded body
+app.use(express.json()); // cho JSON (application/json)
+app.use(express.urlencoded({ extended: true })); // cho form (application/x-www-form-urlencoded)
 
-app.use((req, res, next) => {
-  // Nếu là multipart/form-data (upload file), bỏ qua express.json
-  if (req.is("multipart/form-data")) return next();
-  express.json()(req, res, next); // giải mã JSON body từ client gửi lên => req.body
-});
-
-// Export app để server.js dùng
 module.exports = app;
