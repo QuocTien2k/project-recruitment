3e789dfc05a120388764503eb296955345174ac5
QuocTien2k <hadesdqt@gmail.com>
2025-05-09 10:25:30 +0700

    config app.js for multipart

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-// Cho phép CORS với frontend (React)
+// Cho phép CORS với frontend (React) middleware
 app.use(
   cors({
     origin: "http://localhost:5173", // Chỉ cho phép React frontend
@@ -11,11 +11,14 @@ app.use(
 );
 app.use(cors());
 
-// Parse JSON bodies (as sent by API clients)
-app.use(express.json());
-
-// Parse URL-encoded bodies (as sent by HTML forms)
+// giải mã urlencoded body từ client gửi lên (dạng form)
 app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  // Nếu là multipart/form-data (upload file), bỏ qua express.json
+  if (req.is("multipart/form-data")) return next();
+  express.json()(req, res, next); // giải mã JSON body từ client gửi lên => req.body
+});
 
 // Export app để server.js dùng
 module.exports = app;
