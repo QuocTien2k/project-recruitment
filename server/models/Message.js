d4579682f7a2c5961c701dca92bbdcb7c325eaf0
QuocTien2k <hadesdqt@gmail.com>
2025-05-09 11:31:55 +0700

    create model Message.js

---
+++ server/models/Message.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const messageSchema = new mongoose.Schema(
+  {
+    chatId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Chat",
+      required: true, // Bắt buộc phải có chatId
+    },
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true, // Bắt buộc phải có người gửi
+    },
+    text: {
+      type: String,
+      //required: [true, "Tin nhắn không được để trống"], // Thêm thông báo lỗi
+      required: false,
+      trim: true,
+    },
+    read: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Message", messageSchema);
