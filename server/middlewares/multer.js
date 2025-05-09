ee708ff058b8dead984ebe1373052f983f3f0d12
QuocTien2k <hadesdqt@gmail.com>
2025-05-09 10:52:03 +0700

    update config multer.js

--- server/middlewares/multer.js
+++ server/middlewares/multer.js
@@ -1,11 +1,16 @@
 const multer = require("multer");
 
 // Set up multer storage configuration
+// cb: callback function, null means no error
 const storage = multer.diskStorage({
-  filename: function (req, file, cd) {
-    cd(null, file.originalname);
+  destination: function (req, file, cb) {
+    cb(null, "../uploads/"); // Thư mục tạm để chứa file trước khi upload lên Cloudinary
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname); // Giữ tên gốc
   },
 });
+
 const upload = multer({ storage: storage });
 
 module.exports = upload;
