4fa9cbabede0cf2c1f7ae916ee37ce09a53e9025
QuocTien2k <hadesdqt@gmail.com>
2025-05-09 15:41:37 +0700

    custom authMiddleware.js and update config .env

---
+++ server/middlewares/authMiddleware.js
@@ -0,0 +1,74 @@
+const jwt = require("jsonwebtoken");
+
+//xac thực token
+const protect = (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(401).send({
+        message: "Không có token",
+        success: false,
+      });
+    }
+
+    // Lấy token sau "Bearer "
+    const token = authHeader.split(" ")[1];
+
+    // Xác thực token
+    const decodedToken = jwt.verify(token, process.env.SECRET_KEY); // { userId: user._id }
+
+    // Sau khi verify, lưu toàn bộ thông tin decode
+    req.user = decodedToken;
+
+    next(); // Tiếp tục xử lý request
+  } catch (error) {
+    // Kiểm tra lỗi từ jwt.verify
+    if (error.name === "TokenExpiredError") {
+      return res.status(401).send({
+        message: "Token hết hạn! Vui lòng đăng nhập lại",
+        success: false,
+      });
+    }
+    if (error.name === "JsonWebTokenError") {
+      return res.status(401).send({
+        message: "Token không hợp lệ",
+        success: false,
+      });
+    }
+
+    res.status(401).send({
+      message: "Lỗi token: " + error.message,
+      success: false,
+    });
+  }
+};
+
+// Middleware kiểm tra vai trò cụ thể
+const isAdmin = (req, res, next) => {
+  if (req.user?.role !== "admin") {
+    return res
+      .status(403)
+      .json({ message: "Không có quyền admin", success: false });
+  }
+  next();
+};
+
+const isTeacher = (req, res, next) => {
+  if (req.user?.role !== "teacher") {
+    return res
+      .status(403)
+      .json({ message: "Không có quyền teacher", success: false });
+  }
+  next();
+};
+
+const isUser = (req, res, next) => {
+  if (req.user?.role !== "user") {
+    return res
+      .status(403)
+      .json({ message: "Không có quyền user", success: false });
+  }
+  next();
+};
+
+module.exports = { protect, isAdmin, isTeacher, isUser };
