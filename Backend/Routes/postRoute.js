ecf0ee70584698fbd44acd48240785ddd053cb7b
QuocTien2k <hadesdqt@gmail.com>
2025-05-20 11:31:47 +0700

    create+custom API,Route get detail post

--- Backend/Routes/postRoute.js
+++ Backend/Routes/postRoute.js
@@ -3,6 +3,7 @@ const {
   createPost,
   updatePost,
   getMyPosts,
+  getDetailPost,
   deletePost,
 } = require("../controllers/postController");
 const { protect, isUser } = require("../middlewares/authMiddleware");
@@ -10,6 +11,7 @@ const { protect, isUser } = require("../middlewares/authMiddleware");
 router.post("/create-post", protect, isUser, createPost);
 router.put("/update-post/:postId", protect, isUser, updatePost);
 router.get("/my-posts", protect, isUser, getMyPosts);
+router.get("/detail-post", getDetailPost);
 router.delete("/delete-post/:postId", protect, isUser, deletePost);
 
 module.exports = router;
--- Backend/controllers/postController.js
+++ Backend/controllers/postController.js
@@ -163,6 +163,45 @@ const getMyPosts = async (req, res) => {
   }
 };
 
+//lấy bài viết tạo bởi user và thông tin user
+const getDetailPost = async (req, res) => {
+  try {
+    const postId = req.params.postId;
+
+    const post = await PostModel.findById(postId).populate(
+      "createdBy",
+      "middleName name email profilePic"
+    );
+    if (!post) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Bài viết không tồn tại." });
+    }
+    if (!post.createdBy) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Người tạo bài viết không tồn tại." });
+    }
+
+    const fullName = `${post.createdBy.middleName} ${post.createdBy.name}`;
+
+    res.status(200).json({
+      success: true,
+      data: {
+        ...post._doc,
+        createdBy: {
+          ...post.createdBy._doc,
+          fullName,
+        },
+      },
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ success: false, message: "Lỗi server: " + error.message });
+  }
+};
+
 //xóa bài tuyển dụng của chính mình
 const deletePost = async (req, res) => {
   try {
@@ -200,5 +239,6 @@ module.exports = {
   createPost,
   updatePost,
   getMyPosts,
+  getDetailPost,
   deletePost,
 };
