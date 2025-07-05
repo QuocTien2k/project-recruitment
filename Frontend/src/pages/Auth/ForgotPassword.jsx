d384808235ba743ec9359a7e6defccb4d1c1c1bd
QuocTien2k <hadesdqt@gmail.com>
2025-07-05 12:30:10 +0700

    custom Ui forgot password

--- Frontend/src/pages/Auth/ForgotPassword.jsx
+++ Frontend/src/pages/Auth/ForgotPassword.jsx
@@ -1,7 +1,73 @@
-import React from "react";
+import { useState } from "react";
+import Button from "../../components/Button";
 
 const ForgotPassword = () => {
-  return <div>ForgotPassword</div>;
+  const [formData, setFormData] = useState({ email: "" });
+  const [loading, setLoading] = useState(false);
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+
+    setErrors((prev) => ({ ...prev, [name]: undefined }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { email } = formData;
+    const newErrors = {};
+
+    //Kiểm tra rỗng
+    if (!email) {
+      newErrors.email = "Vui lòng nhập email!";
+    }
+
+    //kiểm tra định dạng email
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (email && !emailRegex.test(email)) {
+      newErrors.email = "Email không đúng định dạng";
+    }
+
+    // Nếu có lỗi → set errors và dừng
+    if (Object.keys(newErrors).length > 0) {
+      setErrors(newErrors);
+      return;
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 px-4">
+      <div className="w-full max-w-md bg-white p-8 rounded shadow-md">
+        <h2 className="text-2xl font-bold mb-6 text-center text-gray-800">
+          Quên mật khẩu
+        </h2>
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <input
+            id="email"
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md focus:ring focus:ring-blue-200 focus:outline-none"
+            placeholder="example@gmail.com"
+          />
+          {errors.email && (
+            <span className="text-red-500 text-sm">{errors.email}</span>
+          )}
+
+          <div className="flex justify-center">
+            <Button type="submit" disabled={loading}>
+              {loading ? "Đang xử lý..." : "Đặt lại mật khẩu"}
+            </Button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default ForgotPassword;
