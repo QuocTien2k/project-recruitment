2633af9e3954c994ff8ae34d5e8519bc967bfdaf
QuocTien2k <hadesdqt@gmail.com>
2025-07-17 10:07:50 +0700

    create+custom api get info by id

--- Backend/Routes/userRoute.js
+++ Backend/Routes/userRoute.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
-const { getLogged } = require("../controllers/userController");
+const { getLogged, getUserById } = require("../controllers/userController");
 const { protect } = require("../middlewares/authMiddleware");
 
 router.get("/get-logged-user", protect, getLogged);
+router.get("/info-user/:id", protect, getUserById);
 
 module.exports = router;
--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -24,6 +24,34 @@ const getLogged = async (req, res) => {
   }
 };
 
+//lấy thông tin dựa vào id
+const getUserById = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const user = await UserModel.findById(id).select("-password -__v");
+
+    if (!user) {
+      return res.status(404).json({
+        message: "Không tìm thấy người dùng",
+        success: false,
+      });
+    }
+
+    res.status(200).json({
+      message: "Lấy thông tin người dùng thành công",
+      success: true,
+      data: user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Lỗi server: " + error.message,
+      success: false,
+    });
+  }
+};
+
 module.exports = {
   getLogged,
+  getUserById,
 };
