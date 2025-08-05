cd5d30e8be276e0685331ccc2b39d6cc9f39dd29
QuocTien2k <hadesdqt@gmail.com>
2025-08-05 11:18:18 +0700

    update put -> patch

--- Backend/Routes/postRoute.js
+++ Backend/Routes/postRoute.js
@@ -8,7 +8,7 @@ const {
 const { protect, isUser } = require("../middlewares/authMiddleware");
 
 router.post("/create-post", protect, isUser, createPost);
-router.put("/update-post/:postId", protect, isUser, updatePost);
+router.patch("/update-post/:postId", protect, isUser, updatePost);
 router.get("/my-posts", protect, isUser, getMyPosts);
 router.delete("/delete-post/:postId", protect, isUser, deletePost);
 
