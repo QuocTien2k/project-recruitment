c9092f95e608a0f13a9a264c488e10d6a2a7c73b
QuocTien2k <hadesdqt@gmail.com>
2025-08-06 11:52:34 +0700

    upadate logic DynamicTitle

--- Frontend/src/context/DynamicTitle.jsx
+++ Frontend/src/context/DynamicTitle.jsx
@@ -1,21 +1,30 @@
 import { useLocation } from "react-router-dom";
 import { Helmet } from "react-helmet-async";
+import { useMemo } from "react";
 
 const DynamicTitle = () => {
   const location = useLocation();
   const path = location.pathname;
 
-  const customTitleMap = [
-    { match: "/admin", title: "Quản trị" },
-    { match: "/login", title: "Đăng nhập" },
-    { match: "/signup", title: "Đăng ký" },
-    { match: "/forgot-password", title: "Quên mật khẩu" },
-    { match: "/reset-password", title: "Cập nhật mật khẩu" },
-    { match: "/", title: "Trang chủ" },
-  ];
+  const title = useMemo(() => {
+    if (path === "/") return "Trang chủ";
 
-  const matched = customTitleMap.find(({ match }) => path.startsWith(match));
-  const title = matched ? matched.title : "Thuê gia sư";
+    if (path === "/dang-nhap") return "Đăng nhập";
+    if (path === "/dang-ky") return "Đăng ký";
+    if (path === "/quen-mat-khau") return "Quên mật khẩu";
+    if (path === "/khoi-phuc-mat-khau") return "Khôi phục mật khẩu";
+
+    if (path === "/bai-viet-cua-toi") return "Bài viết của tôi";
+    if (path.startsWith("/bai-viet/")) return "Chi tiết bài viết";
+    if (path.startsWith("/giao-vien/")) return "Chi tiết giáo viên";
+
+    if (path === "/admin") return "Trang quản trị";
+
+    if (path === "/unauthorized") return "Không có quyền truy cập";
+    if (path === "*") return "Không tìm thấy trang";
+
+    return "Thuê gia sư | Tìm giáo viên uy tín";
+  }, [path]);
 
   return (
     <Helmet>
@@ -23,5 +32,4 @@ const DynamicTitle = () => {
     </Helmet>
   );
 };
-
 export default DynamicTitle;
