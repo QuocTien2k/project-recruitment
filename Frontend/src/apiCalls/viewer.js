ef4a9cb46d470f130edc986772f2e4cb571a5a81
QuocTien2k <hadesdqt@gmail.com>
2025-08-11 21:25:18 +0700

    call api recordPostView

---
+++ Frontend/src/apiCalls/viewer.js
@@ -0,0 +1,12 @@
+import { axiosInstance } from "./index";
+
+// ghi nhận lượt xem
+export const recordPostView = async (id) => {
+  try {
+    const res = await axiosInstance.post(`/api/view/${id}`);
+    return res?.data;
+  } catch (err) {
+    console.log("Có lỗi khi ghi nhận thông tin:", err?.message);
+    throw err;
+  }
+};
