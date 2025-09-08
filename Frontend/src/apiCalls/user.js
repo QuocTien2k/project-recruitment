90ca0de034bf7065f39d13e5de1391df1b080576
QuocTien2k <hadesdqt@gmail.com>
2025-09-08 16:55:38 +0700

    call API, already use

--- Frontend/src/apiCalls/user.js
+++ Frontend/src/apiCalls/user.js
@@ -69,6 +69,7 @@ export const updateInfo = async (formData) => {
   }
 };
 
+/************** Role user ******************** */
 //Lấy danh sách yêu thích
 export const getListFavorite = async (filters = {}) => {
   try {
@@ -113,6 +114,57 @@ export const checkStatusFavorite = async (teacherId) => {
   try {
     const response = await axiosInstance.get(
       `/api/user/favorites/check/${teacherId}`
+    );
+    return response.data;
+  } catch (err) {
+    console.log("Có lỗi khi kiểm tra:", err?.message);
+    throw err;
+  }
+};
+
+/************** Role Teacher ******************** */
+//Lấy danh sách yêu thích
+export const getListSavedPost = async (filters = {}) => {
+  try {
+    const response = await axiosInstance.get("/api/user/saved-posts", {
+      params: filters, // truyền filters lên query
+    });
+    return response.data;
+  } catch (err) {
+    console.log("Có lỗi khi cập nhật:", err?.message);
+    throw err;
+  }
+};
+
+export const addSavePost = async (postId) => {
+  try {
+    const response = await axiosInstance.post("/api/user/add-saved", {
+      postId,
+    });
+    return response.data;
+  } catch (err) {
+    console.log("Có lỗi khi thêm vào yêu thích:", err?.message);
+    throw err;
+  }
+};
+
+export const removeSavePost = async (postId) => {
+  try {
+    const response = await axiosInstance.delete("/api/user/delete-saved", {
+      data: { postId },
+    });
+    return response.data;
+  } catch (err) {
+    console.log("Có lỗi khi xóa:", err?.message);
+    throw err;
+  }
+};
+
+//kiểm tra trạng thái
+export const checkStatusSavePost = async (postId) => {
+  try {
+    const response = await axiosInstance.get(
+      `/api/user/saved-post/check/${postId}`
     );
     return response.data;
   } catch (err) {
